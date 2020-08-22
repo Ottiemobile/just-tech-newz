d2986560b16dd18b69f14711162f3ae408706db4
Ethan Torres <torresethan99@gmail.com>
2020-08-22 00:36:57 -0500

    ignore the problem for now and keep pushing

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
-const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model
 class User extends Model {
-  checkPassword(loginPw){
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
@@ -38,19 +39,17 @@ User.init(
       }
     }
   },
- 
   {
     hooks: {
-      // Set up beforeCreate lifecycle "hook" functionality
-      async beforeCreate(newUserData){
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
+        return newUserData;
       },
 
-      // Set up beforeUpdate lifecycle "hook" functionality
-      async beforeUpdate(updatedUserData){
+      async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          return updatedUserData;
+        return updatedUserData;
       }
     },
     sequelize,
